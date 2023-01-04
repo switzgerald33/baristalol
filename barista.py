af28b6c03d0a10c6dc527050cd190edc66d372d5
switzgerald33 <117537902+switzgerald33@users.noreply.github.com>
2023-01-03 20:26:39 -0600

    Create barista.py

---
+++ barista.py
@@ -0,0 +1,27 @@
+#lickmynuts2times
+##jk
+###enjoy this shitty salesman
+
+name = input("Whats good welcome to my ShitShack of best shit ever\nwho you be? \nwhatchyo name is?\n")
+
+print("lol\n" + name + "\nlol")
+name2 = "Fggt"
+print("nahh youre " + name2 + " now \nlol")
+
+menu = "shitsoups, ballsoups, and ya momma"
+
+print("what do u want " + name2 + "\ngot " + menu)
+order = input()
+print("LOL you would, " + name2)
+input("aight you sure, " + name + "?" "\nlol\n")
+print("btw i felt bad for calling you " + name2)
+print("\nlol\n jk " + name2)
+print("youre buying 100 " + order + " aight?aight \nlol")
+
+price = 250
+
+quantity = 100
+input=()
+print("yeh gimme $" + str(price * quantity) + " for " + order + "\nlol")
+
+print("kkk bye " + name + "\nlol \n\n\n\n\n\n\n\n\nfckngheyBoi")
